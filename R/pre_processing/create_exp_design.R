538f8bc9f65ed644d74a91b53e6a17aa74478ffc
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-02-16 10:10:00 +0000

    Style edits

--- R/pre_processing/create_exp_design.R
+++ R/pre_processing/create_exp_design.R
@@ -1,23 +1,20 @@
-###
-# function for creating the combinations of factors to test
-###
-create.exp.des <- function (parms, Offset)
-{
+create_exp_des <- function (parms, Offset) {
+
   experiment_ID <- parms$Exp
-  no.replicates <- parms$Replicates  
-  
+  no.replicates <- parms$Replicates
+
   CP_val <- parms$CPValues
   estimate_val <- parms$Estimate
   target_level_val <- parms$TargetLevel
-  
-  values <- expand.grid(ID_exp = experiment_ID, 
-                        ID_run = seq_len(no.replicates), 
-                        CP = CP_val, 
+
+  values <- expand.grid(ID_exp = experiment_ID,
+                        ID_run = seq_len(no.replicates),
+                        CP = CP_val,
                         estimate = estimate_val,
                         target_level = target_level_val)
-  
+
   values$ID_run <- seq(from=1+Offset, to=nrow(values)+Offset, by=1)
-  
+
   values
 
 }
