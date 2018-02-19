ce61cd6628c9a24d81ab75eeedc7ef8a5d659cce
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-02-19 16:02:48 +0000

    Allowed different exp des depending on fixed or variable targets (per species)

--- R/create_exp_design.R
+++ R/create_exp_design.R
@@ -2,14 +2,24 @@ create_exp_des <- function(parms) {
 
   experiment_ID <- parms$Exp
   no.replicates <- parms$Replicates
-
-  estimate_val <- parms$Estimate
   target_level_val <- parms$TargetLevel
 
-  expand.grid(ID_exp = experiment_ID,
-              ID_run = seq_len(no.replicates),
-              CP = CP_val,
-              estimate = estimate_val,
-              target_level = target_level_val)
+  fixed_targets <- parms$fixed_targets
 
+  if(fixed_targets){
+
+    out <- expand.grid(ID_exp = experiment_ID,
+                       ID_run = seq_len(no.replicates),
+                       target_level = target_level_val)
+
+  } else {
+
+    out <- expand.grid(ID_exp = experiment_ID,
+                       ID_run = seq_len(no.replicates))
+
+  }
+
+  out$ID_run <- seq_len(nrow(out))
+
+  out
 }
