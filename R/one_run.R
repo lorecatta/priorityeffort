55c4791e2c09ffd2cd5016d3aa624b20b91d82a0
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-02-19 16:03:11 +0000

    Added wrapper

--- R/one_run.R
+++ R/one_run.R
@@ -1,3 +1,51 @@
+wrapper <- function(parms,
+                    exp_des,
+                    site_threat_array,
+                    planning_unit,
+                    cons_feat_array,
+                    site_species_array,
+                    species_responses,
+                    parallel){
+
+  fixed_targets <- parms$fixed_targets
+
+  if(!fixed_targets){
+
+    cons_feat_array <- set_scaled_targets(cons_feat_array,
+                                          occurrence_limits,
+                                          target_limits)
+  }
+
+  exp_des_ls <- df_to_list(exp_des, use_names = TRUE)
+
+  if(parallel){
+
+    n_cores <- parallel::detectCores() - 1
+
+    context::parallel_cluster_start(n_cores, ctx)
+
+  }
+
+  out <- loop(exp_des_ls,
+              one_run,
+              parms,
+              site_threat_array,
+              planning_unit,
+              cons_feat_array,
+              site_species_array,
+              species_responses,
+              parallel = parallel)
+
+  if(parallel){
+
+    context::parallel_cluster_stop()
+
+  }
+
+  out
+
+}
+
 one_run <- function(x,
                     parms,
                     site_threat_array,
@@ -8,14 +56,13 @@ one_run <- function(x,
 
   #browser()
 
+  fixed_targets <- parms$fixed_targets
+
   ID_exp <- x$ID_exp
   cat("ID experiment =", ID_exp, "\n")
 
   ID_run <- x$ID_run
   cat("ID run =", ID_run, "\n")
-
-  target_level <- x$target_level
-  cat("target level =", target_level, "\n")
 
   estimate <- 1 # at the moment we use experts' best estimates for the prioritization
 
@@ -23,73 +70,62 @@ one_run <- function(x,
   all_site_action_int_combs <- get.site.action.intensities.combs(parms, site_threat_array_cat)
   action_costs <- get.action.costs(site_threat_array_cat, planning_unit)
   responses_to_actions <- get.responses.to.actions(species_responses, cons_feat_array, estimate)
-  responses_to_actions.best_guesses <- get.responses.to.actions(species_responses, cons_feat_array, 1)
+  responses_to_actions.best_guesses <- get.responses.to.actions(species_responses, cons_feat_array, estimate = 1)
   required_actions <- get.required_actions(site_threat_array_cat, responses_to_actions.best_guesses, cons_feat_array)
 
   site_action_array <- site_threat_array_cat
   site_action_array[] <- 0
 
-  # set targets
-  cons_feat_array <- set_fixed_targets(cons_feat_array, site_species_array, target_level)
+  if(fixed_targets){
+
+    target_level <- x$target_level
+    cat("target level =", target_level, "\n")
+
+    cons_feat_array <- set_fixed_targets(cons_feat_array, site_species_array, target_level)
+
+  }
 
   # run the optimization
-  run <- Optimize(
-    cons_feat_array,
-    all_site_action_int_combs,
-    site_action_array,
-    action_costs,
-    site_threat_array_cat,
-    responses_to_actions,
-    site_species_array,
-    all_upstream_connections,
-    boundary.file,
-    required_actions,
-    CP,
-    all_downstream_connections)
+  run <- Optimize(parms,
+                  cons_feat_array,
+                  all_site_action_int_combs,
+                  site_action_array,
+                  action_costs,
+                  site_threat_array_cat,
+                  responses_to_actions,
+                  site_species_array,
+                  required_actions)
 
-  initial_cost <- as.vector(run[[1]]["CostCount"])
   ann_summary <- run[[2]]
   site_action_array <- run[[3]]
   SpeciesBenefit_mat <- run[[4]]
   final_species_benefit <- sum(SpeciesBenefit_mat)
 
-  final_cost <- as.vector(ann_summary[nrow(ann_summary),"CostCount"])
-  final_species_penalty <- as.vector(ann_summary[nrow(ann_summary),"PenaltyCount"])
-  final_conn_penalty <- as.vector(ann_summary[nrow(ann_summary),"ConnPenalty"])
-  final_obj_fun_value <- as.vector(ann_summary[nrow(ann_summary),"OF"])
+  final_cost <- as.vector(ann_summary[nrow(ann_summary), "CostCount"])
+  final_species_penalty <- as.vector(ann_summary[nrow(ann_summary), "PenaltyCount"])
+  final_obj_fun_value <- as.vector(ann_summary[nrow(ann_summary), "OF"])
 
   no_PUs <- length(which(apply(site_action_array, 1, function(x) min(sum(x),1))==1))
 
-  # plot temperature, costs and penalties
-  plot.optimize.output(ann_summary, ID_exp, ID_run)
+  # # write out the annealing summary
+  # out_pth <- file.path("output", paste("exp", ID_exp, sep="_"), "annealing_summaries")
+  # dir.create(out_pth, FALSE, TRUE)
+  # annealing_summary_name <- sprintf("annealing_summary_%s%s", paste("run", ID_run, sep="_"), ".rds")
+  # saveRDS(ann_summary, file.path(out_pth, annealing_summary_name))
 
-  # write out the annealing summary
-  #dir.create(file.path("output", paste("output_exp", ID_exp, sep="_"), "annealing_summaries"), FALSE, TRUE)
-  #annealing_summary_name <- sprintf("annealing_summary_%s%s", paste("run", ID_run, sep="_"), ".rds")
-  #saveRDS(ann_summary, file.path("output", paste("output_exp", ID_exp, sep="_"), "annealing_summaries", annealing_summary_name))
+  output <- list()
 
-  # write out the site_action_array
-  #dir.create(file.path("output", paste("output_exp", ID_exp, sep="_"), "best_solutions"), FALSE, TRUE)
-  #site_action_array_name <- sprintf("site_action_array_%s%s", paste("run", ID_run, sep="_"), ".rds")
-  #saveRDS(site_action_array, file.path("output", paste("output_exp", ID_exp, sep="_"), "best_solutions", site_action_array_name))
-
-  # record outputs
-  output <- vector("list", 10)
-  output [[1]] <- initial_cost
-  output [[2]] <- final_cost                  # needed
-  output [[3]] <- final_species_benefit       # needed
-  output [[4]] <- final_species_penalty       # needed
-  output [[5]] <- final_conn_penalty          # needed
-  output [[6]] <- final_obj_fun_value         # needed
-  output [[7]] <- site_action_array           # needed
-  output [[8]] <- SpeciesBenefit_mat
-  output [[9]] <- no_PUs                      # needed
-  output [[10]] <- cons_feat_array[,"target"] # needed
-
-  # save R workspace
-  #dir.create(file.path("output", paste("output_exp", ID_exp, sep="_"), "R_workspaces"), FALSE, TRUE)
-  #workspace_name <- sprintf("Run_%s%s", ID_run, ".RData")
-  #save(output, file = file.path("output", paste("output_exp", ID_exp, sep="_"), "R_workspaces", workspace_name))
+  output$exp <- ID_exp
+  output$run <- ID_run
+  output$cost <- final_cost                             # needed
+  output$species_benefit <- final_species_benefit       # needed
+  output$species_penalty <- final_species_penalty       # needed
+  output$OF_value <- final_obj_fun_value                # needed
+  output$site_action_array <- site_action_array         # needed
+  output$species_benefit_array <- SpeciesBenefit_mat
+  output$no_pu <- no_PUs                                # needed
+  output$targets <- cons_feat_array[,"target"]          # needed
+  output$summary <- ann_summary                         # needed
 
   output
 }
