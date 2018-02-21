7ae0be9546f4b615d27ccb51ebc8c5d7836bb80e
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-02-21 15:28:28 +0000

    Works fine

--- analysis/6_post_processing_exp_1.R
+++ analysis/6_post_processing_exp_1.R
@@ -1,7 +1,5 @@
 
-library(rgdal)
-library(dplyr)
-library(ggplot2)
+devtools::load_all()
 
 
 # define parameters -----------------------------------------------------------
@@ -9,147 +7,113 @@ library(ggplot2)
 
 parameters <- list(
   Exp = 1,
-  Replicates = 1,
-  no_ITER = 100,
+  Replicates = 10,
+  no_ITER = 1000000,
   Temp_zero = 1,
   cooling_factor = 0.99999,
-  fixed_targets = TRUE,
+  fixed_targets = FALSE,
   occurrence_limits = c(500, 10000),
   target_limits = c(1, 0.1),
-  TargetLevel = c(500, 1000, 1500),# c(seq(50, 1000, 50), seq(1250, 10000, 250))
+  TargetLevel = c(seq(50, 1000, 50), seq(1250, 10000, 250)),
+  spf = 10,
   print_every_iter = 5)
 
-summary_table_name <- sprintf("summary_diagnostics_%s%s", paste("exp", parameters$Exp, sep = "_"), ".csv")
-
-output_fl_nm <- "table_2_for_paper.txt"
+cost_scale_factor <- 10000
 
 
-# ---------------------------------------- load other function
+# load data -------------------------------------------------------------------
 
-
-# load data
-site_species_array_df <- read.csv(file.path("data", "input_files","Planning Unit vs. Conservation Feature.csv"), header=TRUE, sep = ",")
-site_threat_array_df <- read.csv(file.path("data", "input_files", "Planning Unit vs. Threat.csv"), header = TRUE, sep = ",")
-planning_unit_df <- read.csv(file.path("data", "input_files", "Planning Unit.csv"), header = TRUE, sep = ",")
-treated_area_df <- read.csv(file.path("data","Treated_area.csv"), header=TRUE, sep = ",")
-daly_planning_unit_area <- read.table(file.path("data","planning_unit_area_ha.txt"), header = TRUE, sep = ",")
-
-daly_subcatch <- readOGR(dsn = file.path("data", "shapefiles"), layer = "Catch_500")
-daly_rivers <- readOGR(dsn = file.path("data", "shapefiles"), layer = "River_500")
 
 results_exp <- readRDS(
-  file.path("output",
-            paste0("output_exp_", parameters$Exp),
-            paste0("queue_obj_exp_", parameters$Exp, ".rds")))
+  file.path("output", paste0("exp_", parameters$Exp), "solution.rds"))
+
+exp_des <- read.csv(
+  file.path("output", paste0("exp_", parameters$Exp), "experimental_design.csv"),
+  header = TRUE)
 
 
-# ---------------------------------------- start
-
-
-# convert df to matrices
-site_threat_array <- as.matrix (site_threat_array_df)
-planning_unit <- as.matrix (planning_unit_df)
-treated_area <- as.matrix(treated_area_df)
-
-# create experimental design
-exp.des <- create.exp.des (parameters, 0)
-
-
-# ---------------------------------------- create summary table of simulation diagnostics
+# create table of diagnostics -------------------------------------------------
 
 
 # get diagnostics for each run
-run_diagnostics <- sapply(
-  seq_along(results_exp),
-  get_run_diagnostics,
-  all_run_results = results_exp)
+run_diagnostics_ls <- lapply(results_exp, get_run_diagnostics)
 
 # traslate
-run_diagnostics_t <- t(run_diagnostics)
+run_diagnostics <- do.call("rbind", run_diagnostics_ls)
 
 # add columns for additional diagnostics to summary table
-run_diagnostics_t <- cbind(run_diagnostics_t,
-                           best = rep(0, nrow(run_diagnostics_t)),
-                           prop_selected = rep(0, nrow(run_diagnostics_t)))
+run_diagnostics <- cbind(run_diagnostics,
+                         best = rep(0, nrow(run_diagnostics)),
+                         prop_selected = rep(0, nrow(run_diagnostics)))
 
 # get indices of best runs
-indices_best_runs <- moving_index_search(run_diagnostics_t, parameters$Replicates)
+indices_best_runs <- moving_index_search(run_diagnostics, parameters$Replicates)
 
 # flag best runs in summary table
-run_diagnostics_t[indices_best_runs, "best"] <- 1
+run_diagnostics[indices_best_runs, "best"] <- 1
 
 # get the index of selected planning units
 selected_planning_unit_indices <- lapply(results_exp, get_selected_pu_index)
 
-# get the total area of the Daly catchment
-total_area <- sum(daly_planning_unit_area[, "Shape_Area"])
+# calculate the proportion of the catchment which is selected
+prop_daly_selected <- vapply(selected_planning_unit_indices,
+                             calc_daly_prop_selected,
+                             1,
+                             treated_area)
 
-# add proportion of total area which has been selected
-run_diagnostics_t[, "prop_selected"] <- sapply(
-  seq_along(results_exp),
-  calc_daly_prop_selected,
-  selected_planning_unit_indices,
-  daly_planning_unit_area,
-  total_area)
+# add to summary table
+run_diagnostics[, "prop_selected"] <- prop_daly_selected
 
-# scale back costs to original scale
-run_diagnostics_t[, "cost"] <- run_diagnostics_t[, "cost"] / 100
+# convert costs to millions
+run_diagnostics[, "cost"] <- (run_diagnostics[, "cost"] * cost_scale_factor) / 1e6
 
 # append summary table to exp design
-run_diagnostics_t_2 <- cbind(exp.des, run_diagnostics_t)
+run_diagnostics_2 <- cbind(exp_des, run_diagnostics)
+
+best_run_id <- which(run_diagnostics_2[, "best"] == 1)
 
 # get only the best solutions
-output_summary_best <- as.data.frame(run_diagnostics_t_2[which(run_diagnostics_t_2[, "best"] == 1), ])
+best_run <- run_diagnostics_2[best_run_id, ]
 
 
-# ---------------------------------------- save output
+# save summary table ----------------------------------------------------------
 
 
-# write out summary table
-write_out_csv(output_summary_best,
-              file.path("output", paste("output_exp", parameters$Exp, sep="_")),
-              summary_table_name)
+write_out_csv(best_run,
+              file.path("output", paste0("exp_", parameters$Exp)),
+              "summary_table.csv")
 
 
-# ---------------------------------------- create table of cost breakdown
+# create table of cost breakdown ----------------------------------------------
 
 
-ID_I_want_to_plot <- 2
+best_solution <- results_exp[[best_run_id]]$site_action_array
 
-best_solution <- results_exp[[ID_I_want_to_plot]][[7]]
-
-site_threat_array_cat <- get.threat.intensity.category(parameters, site_threat_array)
+site_threat_array_cat <- get.threat.intensity.category(parameters,
+                                                       site_threat_array)
 
 action_costs <- get.action.costs(site_threat_array_cat, planning_unit)
 
-action_intensity_scaling_factors <- get.action.costs(site_threat_array_cat)
+# action intensity scaling factors
+scaling_factors <- get.action.costs(site_threat_array_cat)
 
-table_2 <- get_cost_breakdown(site_action_array = best_solution,
-                              treated_area_file = treated_area,
-                              action_cost_list = action_costs,
-                              scaling_factors = action_intensity_scaling_factors)
+paper_table <- get_cost_breakdown(site_action_array = best_solution,
+                                  treated_area_file = treated_area,
+                                  action_cost_list = action_costs,
+                                  scaling_factors = scaling_factors)
 
 # scale back costs to original scale
-cost_based_diagnostics <- grep("cost", names(table_2))
-table_2[, cost_based_diagnostics] <- table_2[, cost_based_diagnostics] / 100
+cost_diagnostics <- grep("cost", names(paper_table))
+paper_table[, cost_diagnostics] <- (paper_table[, cost_diagnostics] * cost_scale_factor) / 1e6
 
 # convert ha to km2
-area_based_diagnostics <- grep("area|extent", names(table_2))
-table_2[, area_based_diagnostics] <- table_2[, area_based_diagnostics] / 100
+area_diagnostics <- grep("area|extent", names(paper_table))
+paper_table[, area_diagnostics] <- paper_table[, area_diagnostics] / 100
 
 
-# ---------------------------------------- write out outputs
+# save table for paper --------------------------------------------------------
 
 
-# create folders for species summaries
-dir.create(file.path("output",
-                     paste("output_exp", parameters$Exp, sep="_")),
-           FALSE, TRUE)
-
-# write out cost breakdown table
-write.table(table_2,
-            file.path("output",
-                      paste("output_exp", parameters$Exp, sep = "_"),
-                      output_fl_nm),
-            row.names = FALSE,
-            sep = ",")
+write_out_txt(paper_table,
+              file.path("output", paste0("exp_", parameters$Exp)),
+              "table_for_paper.txt")
