63fafda4e4778c32ae95ea83c32a1692ab361d50
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-02-23 14:17:28 +0000

    Changed image size and resolution

--- R/plot_representation_error_exp_1.R
+++ R/plot_representation_error_exp_1.R
@@ -8,28 +8,29 @@ bar_plot_representation_error <- function(aa, parms){
 
   plot.file.name <- "Representation_error.png"
 
-  #se_values <- aa$se_mean_perc_change
   y_values <- pretty(aa$mean_perc_change, n = 10)
-  #x_values <- pretty(aa$target_level, n = 10)
 
   png(file.path(out_pth, plot.file.name),
-       width = 4,
-       height = 4,
-      units = "in",
-      res = 300)
+      width = 8,
+      height = 8,
+      units = "cm",
+      res = 600)
 
   p <- ggplot(aa, aes(x = response_type, y = mean_perc_change)) +
     geom_bar(stat = "identity", width = 0.5) +
-    geom_errorbar(aes(ymin = mean_perc_change - se_mean_perc_change, ymax = mean_perc_change + se_mean_perc_change),
+    geom_errorbar(aes(ymin = mean_perc_change - se_mean_perc_change,
+                      ymax = mean_perc_change + se_mean_perc_change),
                   width = .15) +
     #labs(colour = "True response") +
     geom_abline(intercept = 0, slope = 0, linetype = 1) +
-    scale_x_discrete("True response", labels = c("Best guess", "Lower bound", "Upper bound")) +
+    scale_x_discrete("True response", labels = c("Best guess",
+                                                 "Lower bound",
+                                                 "Upper bound")) +
     scale_y_continuous("Change in species representation") +
-    theme(axis.text.x=element_text(angle = 0, vjust = 1, hjust = 0.5),
-          axis.title.x=element_text(vjust = 0.5),
-          axis.title.y=element_text(vjust = 0.5),
-          plot.margin=unit(c(0.5,0.5,0.5,0.5), "cm"))
+    theme(axis.text.x = element_text(size = 8,angle = 0, vjust = 1, hjust = 0.5),
+          axis.title.x = element_text(vjust = 0.5, margin = margin(t = 10)),
+          axis.title.y = element_text(vjust = 0.5),
+          plot.margin = unit(c(0.2, 0.2, 0.2, 0.2), "cm"))
 
   print(p)
 
